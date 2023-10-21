8cfb94ba2c2dc2111f09ef06788a11461d6ef3ac
Gamaliel Adun <adungamaliel@gmail.com>
2023-10-21 22:02:43 +0100

    init

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Simple helper function
+"""
+
+import csv
+
+
+def index_range(page=None, page_size=None):
+    """
+    Simple helper function for pagination
+
+    Args:
+        page (int): page number (1-indexed)
+        page_size(int): page size
+
+    Returns:
+        (start_index, end_index) (tuple): Range of indexes to return in a list
+                                          for those particular pagination
+                                          parameters
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return (start_index, end_index)
